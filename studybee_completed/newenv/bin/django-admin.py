e8fee506908f630888129c783765b35839a164b9
kim <rlawjdgh456@naver.com>
2015-12-21 11:56:07 +0900

    blog

---
+++ studybee_completed/newenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/kim/Desktop/webDevForBeginner-master/studybee_completed/newenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
